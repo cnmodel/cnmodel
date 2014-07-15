cell defnitions: Changes to structure of mod files, computation of resting potential, initialization and protocol. Tstellate cell appears correct; bushy cell not yet finding correct vm0.

nrnlibrary/cells/bushy.py
@@ -2,6 +2,7 @@ from neuron import h
 import neuron as nrn
 from ..pynrnutilities import nstomho
 import numpy as np
+import scipy.optimize
 
 from .cell import Cell
 
@@ -14,7 +15,7 @@ class Bushy(Cell):
     Rothman and Manis, 2003abc (Type II)    
     """
 
-    def __init__(self, nach='jsrna', ttx=False, debug=False):
+    def __init__(self, nach='nacn', ttx=False, debug=False):
         """
         initialize the bushy cell, using the default parameters for guinea pig from
         R&M2003, as a type II cell.
@@ -26,10 +27,12 @@ class Bushy(Cell):
                        'na': nach, 'species': 'guineapig-bushy-II', 'ttx': ttx}
         self.e_k = -70  # potassium reversal potential, mV
         self.e_na = 55
+        self.e_h = -43
         self.c_m = 0.9  # specific membrane capacitance,  uf/cm^2
         self.R_a = 150  # axial resistivity of cytoplasm/axoplasm, ohm.cm
-        self.vm0 = -63.6   # nominal for type II; will be reset below
+        self.vm0 = -63.6467358   # nominal for type II
         self.i_test_range=(-0.5, 0.5, 0.05)
+        self.e_leak = -65
 
         soma = h.Section()  # one compartment of about 29000 um2
         soma.nseg = 1
@@ -38,21 +41,78 @@ class Bushy(Cell):
         soma.insert('ihvcn')
         soma.insert('leak')
         soma.ek = self.e_k
-        soma().leak.e = -65.0
+        soma().ihvcn.eh = self.e_h
+        soma().leak.erev = self.e_leak
 
         # insert the requested sodium channel
         if nach == 'jsrna':
             soma.insert('jsrna')
         elif nach == 'nav11':
             soma.insert('nav11')
+        elif nach in ['na', 'nacn']:
+            soma.insert('na')
         else:
-            soma.insert('nacn')
+            raise ValueError('Sodium channel %s not available for Bushy cells' % nach)
+
+        soma.ena = self.e_na
         self.soma = soma
         self.species_scaling()  # set the default type II cell parameters
         self.all_sections['soma'].append(soma)
         self.add_section(soma)
         if debug:
             print "<< bushy: JSR bushy cell model created >>"
+
+    def find_i0(self, vrange=[-70., -55.]):
+        v0 = scipy.optimize.brentq(self.i_bushy, vrange[0], vrange[1])
+        #print 'found V0 = %f' % v0
+        return v0
+
+    def i_bushy(self, V):
+        """
+        For the steady-state case, return the total current at voltage V
+        Used to find the zero current point
+        vrange brackets the interval
+
+        """
+        ix = {}
+
+        # klt
+        zss = 0.5   # <0,1>   : steady state inactivation of glt
+        q10g = 2.0
+        qg = q10g**((h.celsius-22.)/10.)
+        winf = (1.0 / (1.0 + np.exp(-(V + 48.) / 6 )))**0.25
+        zinf = zss + ((1.0-zss) / (1.0 + np.exp((V + 71.) / 10 )))
+        gklt = qg*self.soma().klt.gbar*(winf**4.)*zinf
+        ix['klt'] = gklt*(V - self.e_k)
+
+        # ihvcn
+        q10g = 2.0
+        qg = q10g**((h.celsius-22.)/10.)
+        rinf = 1. / (1+np.exp((V + 76.) / 7.))
+        gh = qg*self.soma().ihvcn.gbar*rinf
+        ix['ih'] = gh*(V - self.e_h)
+
+        # kht
+        q10g = 2.0
+        qg = q10g**((h.celsius-22)/10)
+        nf = 0.85  # proportion of n vs p kinetics
+        ninf =   (1 + np.exp(-(V + 15.) / 5.))**(-0.5)
+        pinf =  1 / (1 + np.exp(-(V + 23.) / 6.))
+        gkht = qg*self.soma().kht.gbar*(nf*(ninf**2) + (1.-nf)*pinf)
+        ix['kht'] = gkht*(V - self.e_k)
+
+        # nacn
+        q10g = 2.0
+        qg = q10g**((h.celsius-22.)/10.)
+        minf = 1 / (1+np.exp(-(V + 38.) / 7.))
+        hinf = 1 / (1+np.exp((V + 65.) / 6.))
+        gna = qg*self.soma().na.gbar*(minf**3.0)*hinf
+        ix['na'] = gna*(V - self.e_na)
+
+        # leak
+        ix['leak'] = self.soma().leak.gbar*(V - self.e_leak)
+#        print ix
+        return np.sum([ix[i] for i in ix])
 
     def set_soma_size_from_Cm(self, cap):
         self.totcap = cap
@@ -75,7 +135,6 @@ class Bushy(Cell):
             soma().klt.gbar = nstomho(80.0, self.somaarea)
             soma().ihvcn.gbar = nstomho(30.0, self.somaarea)
             soma().leak.gbar = nstomho(2.0, self.somaarea)
-            soma().leak.e = -65.0
             self.vm0 = -63.6
             self.axonsf = 0.57
         elif species == 'guineapig-bushy-II':
@@ -85,9 +144,9 @@ class Bushy(Cell):
             soma().klt.gbar = nstomho(200.0, self.somaarea)
             soma().ihvcn.gbar = nstomho(20.0, self.somaarea)
             soma().leak.gbar = nstomho(2.0, self.somaarea)
-            soma().leak.e = -65.0
-            self.vm0 = -63.6
+            self.vm0 = self.find_i0() # -63.6467358  # "-63.6"
             self.axonsf = 0.57
+            print 'set cell as: ', species
         elif species == 'guineapig-bushy-II-I':
             # guinea pig data from Rothman and Manis, 2003, type II=I
             self.set_soma_size_from_Cm(12.0)
@@ -96,8 +155,7 @@ class Bushy(Cell):
             soma().klt.gbar = nstomho(35.0, self.somaarea)
             soma().ihvcn.gbar = nstomho(3.5, self.somaarea)
             soma().leak.gbar = nstomho(2.0, self.somaarea)
-            soma().leak.e = -65.0
-            self.vm0 = -63.655
+            self.vm0 = -63.648175
             self.axonsf = 0.57
         elif species == 'cat':  # a cat is a big guinea pig
             self.set_soma_size_from_Cm(35.0)
@@ -106,7 +164,6 @@ class Bushy(Cell):
             soma().klt.gbar = nstomho(200.0, self.somaarea)
             soma().ihvcn.gbar = nstomho(20.0, self.somaarea)
             soma().leak.gbar = nstomho(2.0, self.somaarea)
-            soma().leak.e = -65.0
             self.vm0 = -63.6
             self.axonsf = 1.0
         else:
@@ -138,15 +195,16 @@ class Bushy(Cell):
             if debug:
                 print "bushy using inva11"
             print 'nav11 gbar: ', soma().nav11.gbar
-        else:
-            soma().nacn.gbar = gnabar
+        elif nach in ['nacn', 'na']:
+            soma().na.gbar = gnabar
             soma.ena = self.e_na
             if debug:
-                print 'nacn gbar: ', soma().nacn.gbar
+                print 'nacn gbar: ', soma().na.gbar
+        else:
+            raise ValueError('Sodium channel %s is not recognized for Bushy cells', nach)
 
     def add_axon(self):
         Cell.add_axon(self, self.c_m, self.R_a, self.axonsf)
-
 
     def add_pumps(self):
         """

nrnlibrary/protocols.py
@@ -39,6 +39,33 @@ class Protocol(object):
         """
         return np.array(self._vectors[name])
 
+    def custom_init(self):
+        """
+        Perform careful initialization cell states, without forcing
+        a particular Vm.
+        """
+        initdur = -1e10
+        tdt = h.dt
+        dtstep = 1e9
+        h.finitialize()
+        h.t = initdur
+        tmp = h.cvode.active()
+        if tmp != 0:
+            h.cvode.active(0)
+        h.dt = dtstep
+        while h.t < -dtstep:
+            h.fadvance()
+        if tmp != 0:
+            h.cvode.active(1)
+        h.t = 0
+        if h.cvode.active():
+            h.cvode.re_init()
+        else:
+            h.fcurrent()
+        h.frecord_init()
+        h.dt = tdt
+        h.fcurrent()
+        h.frecord_init()
 
 class IVCurve(Protocol):
     def __init__(self):
@@ -108,7 +135,7 @@ class IVCurve(Protocol):
                 'dt': dt,
                 }
         istim = h.iStim(0.5, sec=cell.soma)
-        istim.delay = 0
+        istim.delay = 5.
         istim.dur = 1e9 # these actually do not matter...
         istim.iMax = 0.0
         (secmd, maxt, tstims) = make_pulse(stim)
@@ -144,19 +171,25 @@ class IVCurve(Protocol):
             vec['i_stim'].play(istim._ref_i, h.dt, 0, sec=cell.soma)
 
             # GO
+            h('secondorder=0')  # direct call fails; let hoc do the work
             h.dt = dt
             h.celsius = temp
             h.tstop = tend
             # h.init()
             # h.finitialize(cell.vm0)
             cell.initialize()
-            h.frecord_init()
-            print 'After finitialize: \nvm0: ', cell.vm0
-            print 'soma v: ', cell.soma(0.5).v
-            print 'temp: ', temp
+            self.custom_init()
+            #h.finitialize(cell.vm0)
+            #h.fcurrent()
+            #h.frecord_init()
+            print('After initialization at {0:4.1f}: \n  vm0: {1:9.5f}  soma.v: {2:9.5f}'.format(
+                temp, cell.vm0, cell.soma(0.5).v))
             #h.run()
             while h.t < h.tstop:
                 h.fadvance()
+
+            k1 = int(stim['delay']/h.dt - 1)
+            print ('   V before step: {0:9.5f}'.format(self['v_soma'][k1]))
             self.voltage_traces.append(self['v_soma'])
             self.current_traces.append(self['i_inj'])
             self.time_values = np.array(self['time'])