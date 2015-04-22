575253faed6df8a1c57b01dd6451a3bea67dd52e
Luke Campagnola <luke.campagnola@gmail.com>
2015-04-22 14:07:46 -0400

    clean up glu_psd

--- nrnlibrary/synapses/glu_psd.py
+++ nrnlibrary/synapses/glu_psd.py
@@ -35,16 +35,17 @@ class GluPSD(PSD):
         """
         PSD.__init__(self, section, terminal)
         
-        pre_sec = terminal.section
-        post_sec = section
+        self.pre_sec = terminal.section
+        self.post_sec = section
+        self.terminal = terminal
         
         from .. import cells
         self.AN_Po_Ratio = 23.2917 # ratio of open probabilities for AMPA and NMDAR's at peak currents
         self.AMPA_Max_Po = 0.44727
         self.NMDARatio = 0.0
         
-        self.pre_cell = cells.cell_from_section(pre_sec)
-        self.post_cell = cells.cell_from_section(post_sec)
+        self.pre_cell = cells.cell_from_section(self.pre_sec)
+        self.post_cell = cells.cell_from_section(self.post_sec)
 
         # get AMPA gmax corrected for max open probability
         gmax = ampa_gmax / self.AMPA_Max_Po
@@ -57,13 +58,12 @@ class GluPSD(PSD):
         # 
         clefts = []
         for k in range(0, n_rzones):
-            cl = h.cleftXmtr(0.5, sec=post_sec)
+            cl = h.cleftXmtr(0.5, sec=self.post_sec)
             clefts.append(cl) # cleft
         
         # and then make a set of postsynaptic receptor mechanisms
         #        print 'PSDTYPE: ', psdtype
-        (ampa_psd, nmda_psd, par, parn) = template_iGluR_PSD(post_sec, nReceptors=n_rzones,
-                                                    nmda_ratio=nmda_ratio)
+        (ampa_psd, nmda_psd, par, parn) = template_iGluR_PSD(nmda_ratio=nmda_ratio)
         
         # Connect terminal to psd (or cleft)
         for k in range(0, n_rzones):
@@ -95,57 +95,59 @@ class GluPSD(PSD):
         self.clefts = clefts
         self.par = par
 
+    def template_iGluR_PSD(self, debug=False, cellname=None, message=None, nmda_ratio=1):
+        """
+        Create an ionotropic Glutamate receptor "PSD"
+        Each PSD has receptors for each active zone, which must be matched (connected) to presynaptic
+        terminals. Each PSD recetpor consists of an AMPATRUSSELL and an NMDA_KAMPA receptor
+        Inputs:
+            sec: The template requires a segment to insert the receptors into
+            nReceptors: The number of receptor sites to insert
+            debug: flag for debugging (prints extra information)
+            cellname: Bushy/MNTB/stellate: determines ampa receptor kinetics
+            message: Not used.
+            nmda_ratio: The relative conductance of the open NMDA receptors to the open AMPA receptors.
+        Outputs:
+            (psd, psdn, par, parn)
+            psd is the list of PSDs that were created (AMPA)
+            psdn is the list of NMDA PSDs (same number as psd, just the NMDARs)
+            par: dictionary of AMPAR kinetics as inserted
+            parn: NMDA ratio
+        Side Effecdts: None
+        """
+        sec = self.post_sec
+        nReceptors = self.terminal.n_rzones
+        
+        psd = []
+        psdn = []
+        sec.push()
+        AN_Po_Ratio = 23.2917 # ratio of open probabilities for AMPA and NMDAR's at peak currents
+        for k in range(0, nReceptors):
+            psd.append(h.AMPATRUSSELL(0.5, sec)) # raman/trussell AMPA with rectification
+            psdn.append(h.NMDA_Kampa(0.5, sec)) # Kampa state model NMDA receptors
 
-def template_iGluR_PSD(sec, nReceptors=1, debug=False, cellname=None, message=None, nmda_ratio=1):
-    """
-    Create an ionotropic Glutamate receptor "PSD"
-    Each PSD has receptors for each active zone, which must be matched (connected) to presynaptic
-    terminals. Each PSD recetpor consists of an AMPATRUSSELL and an NMDA_KAMPA receptor
-    Inputs:
-        sec: The template requires a segment to insert the receptors into
-        nReceptors: The number of receptor sites to insert
-        debug: flag for debugging (prints extra information)
-        cellname: Bushy/MNTB/stellate: determines ampa receptor kinetics
-        message: Not used.
-        nmda_ratio: The relative conductance of the open NMDA receptors to the open AMPA receptors.
-    Outputs:
-        (psd, psdn, par, parn)
-        psd is the list of PSDs that were created (AMPA)
-        psdn is the list of NMDA PSDs (same number as psd, just the NMDARs)
-        par: dictionary of AMPAR kinetics as inserted
-        parn: NMDA ratio
-    Side Effecdts: None
-    """
-    psd = []
-    psdn = []
-    sec.push()
-    AN_Po_Ratio = 23.2917 # ratio of open probabilities for AMPA and NMDAR's at peak currents
-    for k in range(0, nReceptors):
-        psd.append(h.AMPATRUSSELL(0.5, sec)) # raman/trussell AMPA with rectification
-        psdn.append(h.NMDA_Kampa(0.5, sec)) # Kampa state model NMDA receptors
+            if cellname in ['bushy', 'MNTB']:
+                psd[-1].Ro1 = 107.85
+                psd[-1].Ro2 = 0.6193
+                psd[-1].Rc1 = 3.678
+                psd[-1].Rc2 = 0.3212
+                gNAR = 0.036 * AN_Po_Ratio * nmda_ratio # 0.36*AN_Po_Ratio*nmda_ratio
+                psdn[-1].gmax = psdn[-1].gmax * gNAR
+                #if k == 0:
+                #    print "Bushy NMDAR set to %8.2f" % psdn[-1].gNAR
+            if cellname == 'stellate':
+                psd[-1].Ro1 = 39.25
+                psd[-1].Ro2 = 4.40
+                psd[-1].Rc1 = 0.667
+                psd[-1].Rc2 = 0.237
+                psd[-1].PA = 0.1
+                gNAR = 1 * AN_Po_Ratio * nmda_ratio
+                psdn[-1].gmax = psdn[-1].gmax * gNAR
 
-        if cellname in ['bushy', 'MNTB']:
-            psd[-1].Ro1 = 107.85
-            psd[-1].Ro2 = 0.6193
-            psd[-1].Rc1 = 3.678
-            psd[-1].Rc2 = 0.3212
-            gNAR = 0.036 * AN_Po_Ratio * nmda_ratio # 0.36*AN_Po_Ratio*nmda_ratio
-            psdn[-1].gmax = psdn[-1].gmax * gNAR
-            #if k == 0:
-            #    print "Bushy NMDAR set to %8.2f" % psdn[-1].gNAR
-        if cellname == 'stellate':
-            psd[-1].Ro1 = 39.25
-            psd[-1].Ro2 = 4.40
-            psd[-1].Rc1 = 0.667
-            psd[-1].Rc2 = 0.237
-            psd[-1].PA = 0.1
-            gNAR = 1 * AN_Po_Ratio * nmda_ratio
-            psdn[-1].gmax = psdn[-1].gmax * gNAR
-
-    h.pop_section()
-    par = {'Ro1': ('r', psd[0].Ro1),
-           'Ro2': ('r', psd[0].Ro2),
-           'Rc1': ('r', psd[0].Rc1),
-           'Rc2': ('r', psd[0].Rc2), }
-    #parn = {'gNAR': ('4', gNAR), }
-    return (psd, psdn, par, {})
+        h.pop_section()
+        par = {'Ro1': ('r', psd[0].Ro1),
+            'Ro2': ('r', psd[0].Ro2),
+            'Rc1': ('r', psd[0].Rc1),
+            'Rc2': ('r', psd[0].Rc2), }
+        #parn = {'gNAR': ('4', gNAR), }
+        return (psd, psdn, par, {})
