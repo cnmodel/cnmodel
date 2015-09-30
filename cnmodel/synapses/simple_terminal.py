873d178d1aae9907241c2571074a550ca1f772c1
Luke Campagnola <luke.campagnola@gmail.com>
2015-09-30 10:37:29 -0400

    minor corrections

--- cnmodel/synapses/simple_terminal.py
+++ cnmodel/synapses/simple_terminal.py
@@ -11,10 +11,11 @@ class SimpleTerminal(Terminal):
         if spike_source is None:
             spike_source = pre_sec(0.5)._ref_v
         self.spike_source = spike_source
+        self.pre_sec = pre_sec
 
     def connect(self, post, weight):
         thresh = -20
         delay = 0.5
-        self.netcon = h.NetCon(self.spike_source, post, thresh, delay, weight)
+        self.netcon = h.NetCon(self.spike_source, post, thresh, delay, weight, sec=self.pre_sec)
         self.netcon.weight[0] = weight
         
