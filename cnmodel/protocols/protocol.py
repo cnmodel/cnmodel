aeb029458814e2a6d2d4638142547693154555c3
Luke Campagnola <luke.campagnola@gmail.com>
2017-07-15 14:24:58 -0700

    restore Protocol.custom_init (as an alias to util.custim_init)

--- cnmodel/protocols/protocol.py
+++ cnmodel/protocols/protocol.py
@@ -1,6 +1,6 @@
 from neuron import h
 import numpy as np
-from ..util import random
+from ..util import random, custom_init
 
 class Protocol(object):
     """
@@ -37,29 +37,5 @@ class Protocol(object):
         """
         return np.array(self._vectors[name])
 
-    # moved to util
-    # def custom_init(self, vinit=-60.):
-    #     """
-    #     perform a custom initialization of the current section to vinit
-    #     """
-    #     initdur = 1e6
-    #     tdt = h.dt
-    #     dtstep = 1e3
-    #     h.finitialize()
-    #     h.t = -initdur
-    #     tmp = h.cvode.active()
-    #     if tmp != 0:
-    #         h.cvode.active(0)
-    #     h.dt = dtstep
-    #     while h.t < 0:
-    #         h.fadvance()
-    #     if tmp != 0:
-    #         h.cvode.active(1)
-    #     h.t = 0
-    #     if h.cvode.active():
-    #         h.cvode.re_init()
-    #     else:
-    #         h.fcurrent()
-    #     h.frecord_init()
-    #     h.dt = tdt
-    #     h.fcurrent()
+    def custom_init(self, vinit=-60.):
+        return custom_init(vinit)
