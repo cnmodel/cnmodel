df3f67496e587b856198be889c490350ed6781df
Paul B. Manis <pmanis@med.unc.edu>
2017-07-16 07:53:45 -0400

    Revert "Doc updates"
    This reverts commit c3c00a447b277d17bc49a87ea2e8e1d0081bff4c.

--- cnmodel/cells/sgc.py
+++ cnmodel/cells/sgc.py
@@ -93,7 +93,10 @@ class DummySGC(SGC):
             be run though a Python-interface direcltly to the Matlab code
             as publicy available, (simulator='matlab'), or can be run through
             Rudnicki's Python interface to the simulator's C code 
-            (simulator='cochlea').
+            (simulator='cochlea'). Requires installation of the modified
+            versions of cochlea and thorns from github.com/pbmanis/cochlea and
+            github.com/pbmanis/thorns. By default, a simulator will be selected
+            based on availability.
         
         """
         self._simulator = simulator
