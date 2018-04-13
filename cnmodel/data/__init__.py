0689db9454c81dd2d3a8dc76f26cbea2b3421b74
Paul B. Manis <pmanis@med.unc.edu>
2018-04-13 08:52:51 -0400

    fix data/__init__.py

--- cnmodel/data/__init__.py
+++ cnmodel/data/__init__.py
@@ -3,6 +3,7 @@ The cnmodel.data package contains information about ion channel densities,
 connectivity, synaptic properties, and population distributions. These values
 are used by the Cell, Synapse, Population, and related classes to determine
 all model construction parameters.
+
 Values are stored in python strings that contain human-readable tables with
 provenance documentation.
 """
