a56d877d8a2f95efedc5862c00424fe3d451a94e
Paul B. Manis <pmanis@med.unc.edu>
2018-04-13 09:04:35 -0400

    fix init again

--- cnmodel/data/__init__.py
+++ cnmodel/data/__init__.py
@@ -9,8 +9,8 @@ provenance documentation.
 """
 
 from ._db import get, get_source, add_table_data, report_changes
+
 from . import connectivity
 from . import synapses
 from . import populations
 from . import ionchannels
-
